0becd26ac3b21b1a84ff9215a1ca47c74bf6c925
JamesMusembi <you@example.com>
2023-03-12 15:54:42 +0300

    modifying models

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_Length=60)
+    question = model.models.TextField(max_Length=400)
+    priority = model.models.CharField( max_length=1, choice=)
